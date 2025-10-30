7400ce526364c7870eb9f410b1348771e009cdbe
Hannah Alexandre <hdalexan@buffalo.edu>
2025-10-30 19:35:41 -0400

    project 4 completed step 2 and testing

--- projects/project-4/assignment/src/scripts/normalize_readings.py
+++ projects/project-4/assignment/src/scripts/normalize_readings.py
@@ -83,7 +83,7 @@ UNIT_MAP = {
     "celsius": "C", "°c": "C", "C": "C",
     "kilogram": "kg", "KG": "kg", "kg": "kg",
     "meter": "m", "M": "m", "m": "m",
-    "pressure": "kpa", "psi":"kpa",
+    "pressure": "kpa", "psi":"kpa", "kpa": "kpa"
 }
 df["unit_label"] = df["unit_label"].str.lower().map(UNIT_MAP).fillna(df["unit_label"])
 
